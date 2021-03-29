b6e7bdc18bf7e61c30c57248411e8611a985af0d
nocticoding <antonio@nocticode.com>
2021-03-30 01:41:57 +0200

    commit inicial

---
+++ config.js
@@ -0,0 +1,11 @@
+export default 
+{
+    "LANGUAGES": {
+        1: "es",
+        2: "pt",
+        3: "en"
+    },
+    "TIME_FOR_ANSWERS": 5000,
+    "QUESTION_LIST_PATH": "./../assets/quiz/questions.json",
+    "TOTAL_QUESTIONS": 10,
+}
